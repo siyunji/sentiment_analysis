783002e7b96b1edc41be91f4430cd306de16305e
Xinchen Zhang <xinchez4@uci.edu>
2020-05-25 00:07:24 -0700

    Modified for the null property

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,44 +1,50 @@
 import { Directive, ElementRef, Input, OnChanges } from "@angular/core";
 
 @Directive({
-  selector: "[appHighlight]"
+  selector: "[appHighlight]",
 })
 export class HighlightDirective implements OnChanges {
   // * how to get the negative, positive content
   @Input()
   label: string;
 
-  constructor( public el: ElementRef) {}
+  constructor(public el: ElementRef) {}
 
   ngOnChanges() {
     this.setLabelStyle();
   }
 
   setLabelStyle() {
-    switch (this.label) {
+    switch (this.label.toLowerCase()) {
       case "positive":
-        this.el.nativeElement.style.backgroundColor = '#99dbaa';
-        this.el.nativeElement.style.margin = 'auto';
-        this.el.nativeElement.style.padding = '5px';
-        this.el.nativeElement.style.borderRadius = '5%';
+        this.el.nativeElement.style.backgroundColor = "#99dbaa";
+        this.el.nativeElement.style.margin = "auto";
+        this.el.nativeElement.style.padding = "5px";
+        this.el.nativeElement.style.borderRadius = "5%";
         break;
       case "negative":
-        this.el.nativeElement.style.backgroundColor = '#ffe799';
-        this.el.nativeElement.style.margin = 'auto';
-        this.el.nativeElement.style.padding = '5px';
-        this.el.nativeElement.style.borderRadius = '5%';
+        this.el.nativeElement.style.backgroundColor = "#ffe799";
+        this.el.nativeElement.style.margin = "auto";
+        this.el.nativeElement.style.padding = "5px";
+        this.el.nativeElement.style.borderRadius = "5%";
         break;
       case "neutral":
-          this.el.nativeElement.style.backgroundColor = '#ecf0f1';
-          this.el.nativeElement.style.margin = 'auto';
-          this.el.nativeElement.style.padding = '5px';
-          this.el.nativeElement.style.borderRadius = '5%';
-          break;
+        this.el.nativeElement.style.backgroundColor = "#ecf0f1";
+        this.el.nativeElement.style.margin = "auto";
+        this.el.nativeElement.style.padding = "5px";
+        this.el.nativeElement.style.borderRadius = "5%";
+        break;
+      case "null":
+        this.el.nativeElement.style.backgroundColor = "#94bae6";
+        this.el.nativeElement.style.margin = "auto";
+        this.el.nativeElement.style.padding = "5px";
+        this.el.nativeElement.style.borderRadius = "5%";
+        break;
       default:
-        this.el.nativeElement.style.backgroundColor = '#ed8380';
-        this.el.nativeElement.style.margin = 'auto';
-        this.el.nativeElement.style.padding = '5px';
-        this.el.nativeElement.style.borderRadius = '5%';
+        this.el.nativeElement.style.backgroundColor = "#ed8380";
+        this.el.nativeElement.style.margin = "auto";
+        this.el.nativeElement.style.padding = "5px";
+        this.el.nativeElement.style.borderRadius = "5%";
     }
   }
 }
