95fe9b7abd8ee8994ba48fcf40a6e202ce5db1fb
Xinchen Zhang <xinchez4@uci.edu>
2020-08-28 09:57:38 +0800

    Updated style

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -15,36 +15,43 @@ export class HighlightDirective implements OnChanges {
   }
 
   setLabelStyle() {
-    switch (this.label.toLowerCase()) {
-      case "positive":
-        this.el.nativeElement.style.backgroundColor = "#99dbaa";
-        this.el.nativeElement.style.margin = "auto";
-        this.el.nativeElement.style.padding = "5px";
-        this.el.nativeElement.style.borderRadius = "5%";
-        break;
-      case "negative":
-        this.el.nativeElement.style.backgroundColor = "#ffe799";
-        this.el.nativeElement.style.margin = "auto";
-        this.el.nativeElement.style.padding = "5px";
-        this.el.nativeElement.style.borderRadius = "5%";
-        break;
-      case "neutral":
-        this.el.nativeElement.style.backgroundColor = "#ecf0f1";
-        this.el.nativeElement.style.margin = "auto";
-        this.el.nativeElement.style.padding = "5px";
-        this.el.nativeElement.style.borderRadius = "5%";
-        break;
-      case "null":
-        this.el.nativeElement.style.backgroundColor = "#94bae6";
-        this.el.nativeElement.style.margin = "auto";
-        this.el.nativeElement.style.padding = "5px";
-        this.el.nativeElement.style.borderRadius = "5%";
-        break;
-      default:
-        this.el.nativeElement.style.backgroundColor = "#ed8380";
-        this.el.nativeElement.style.margin = "auto";
-        this.el.nativeElement.style.padding = "5px";
-        this.el.nativeElement.style.borderRadius = "5%";
+    if (this.label) {
+      switch (this.label.toLowerCase()) {
+        case "positive":
+          this.el.nativeElement.style.backgroundColor = "#99dbaa";
+          this.el.nativeElement.style.margin = "auto";
+          this.el.nativeElement.style.padding = "6px";
+          this.el.nativeElement.style.borderRadius = "5%";
+          this.el.nativeElement.style.width = "30px";
+          break;
+        case "negative":
+          this.el.nativeElement.style.backgroundColor = "#ffe799";
+          this.el.nativeElement.style.margin = "auto";
+          this.el.nativeElement.style.padding = "5px";
+          this.el.nativeElement.style.borderRadius = "5%";
+          this.el.nativeElement.style.width = "30px";
+          break;
+        case "neutral":
+          this.el.nativeElement.style.backgroundColor = "#ecf0f1";
+          this.el.nativeElement.style.margin = "auto";
+          this.el.nativeElement.style.padding = "8px";
+          this.el.nativeElement.style.borderRadius = "5%";
+          this.el.nativeElement.style.width = "30px";
+          break;
+        case "null":
+          this.el.nativeElement.style.backgroundColor = "#94bae6";
+          this.el.nativeElement.style.margin = "auto";
+          this.el.nativeElement.style.padding = "6px";
+          this.el.nativeElement.style.borderRadius = "5%";
+          this.el.nativeElement.style.width = "30px";
+          break;
+        default:
+          this.el.nativeElement.style.backgroundColor = "#ed8380";
+          this.el.nativeElement.style.margin = "auto";
+          this.el.nativeElement.style.padding = "6px";
+          this.el.nativeElement.style.borderRadius = "5%";
+          this.el.nativeElement.style.width = "30px";
+      }
     }
   }
 }
