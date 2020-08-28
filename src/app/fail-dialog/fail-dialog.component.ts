1c0be83e118371aef8f07aa436cf532dab1ec27e
Xinchen Zhang <xinchez4@uci.edu>
2020-08-28 09:53:35 +0800

    Updated dialog width and height

---
+++ src/app/fail-dialog/fail-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fail-dialog',
+  templateUrl: './fail-dialog.component.html',
+  styleUrls: ['./fail-dialog.component.css']
+})
+export class FailDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
